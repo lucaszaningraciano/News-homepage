db44ecdeac6698ac7dd632186ee947dc6ce500e4
LUCAS ZANIN GRACIANO <lucas.zanin.graciano@hotmail.com>
2023-01-28 19:20:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let menu = document.getElementById("menu");
+let menuBar = document.getElementById("menu-bar");
+let menuClose = document.getElementById("menu-close");
+
+function openMenu() {
+  document.getElementById("menu-bar").style.display = "block";
+}
+
+function closeMenu() {
+  document.getElementById("menu-bar").style.display = "none";
+}
